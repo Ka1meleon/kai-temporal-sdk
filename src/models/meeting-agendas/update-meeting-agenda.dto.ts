83f468aa990d6f269939b578f9a6a2a1e6072d94
Jo Vinkenroye <jestersimpps@gmail.com>
2025-05-13 19:53:48 +0200

    remove partials

--- src/models/meeting-agendas/update-meeting-agenda.dto.ts
+++ src/models/meeting-agendas/update-meeting-agenda.dto.ts
@@ -1,3 +1,9 @@
 import { MeetingAgendaDto } from './meeting-agenda.dto';
 
-export class UpdateMeetingAgendaDto implements Partial<MeetingAgendaDto> {}
+export class UpdateMeetingAgendaDto {
+  id?: string;
+  title?: string;
+  categories?: MeetingAgendaDto['categories'];
+  createdAt?: string;
+  updatedAt?: string;
+}
--- src/models/meetings/update-meeting.dto.ts
+++ src/models/meetings/update-meeting.dto.ts
@@ -1,3 +1,17 @@
 import { MeetingDto } from './meeting.dto';
 
-export class UpdateMeetingDto implements Partial<MeetingDto> {}
+export class UpdateMeetingDto {
+  id?: string;
+  title?: string;
+  date?: string;
+  duration?: number;
+  summary?: string;
+  keyPoints?: string[];
+  actionItems?: string[];
+  decisionsMade?: string[];
+  agenda?: MeetingDto['agenda'];
+  transcription?: string;
+  language?: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
